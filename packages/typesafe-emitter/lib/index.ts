6c68863054feca1d1d0f8d10f3783e2ecaf92212
happyeric77 <happyeric77@gmail.com>
2023-04-23 02:29:03 +0900

    Add TypedEventEmitter

--- packages/typesafe-emitter/lib/index.ts
+++ packages/typesafe-emitter/lib/index.ts
@@ -1,5 +1,14 @@
-export class TypedEventEmitter {
-  private eventListeners: Record<string, Array<Function>> = {};
-  on(eventName: string, callback: (payload: unknown) => void) {}
-  emit(eventName: string, payload: unknown) {}
+type ListenerCallback<T extends Array<any>> = (...payload: T) => void;
+
+export class TypedEventEmitter<T extends Record<string, Array<any>>> {
+  private _eventListeners: { [Key in keyof T]?: Set<ListenerCallback<T[Key]>> } = {};
+  on<U extends keyof T>(eventName: U, callback: ListenerCallback<T[U]>) {
+    const listeners = this._eventListeners[eventName] ?? new Set();
+    listeners.add(callback);
+    this._eventListeners[eventName] = listeners;
+  }
+  emit<U extends keyof T>(eventName: U, ...payloads: T[U]) {
+    const callbacks = this._eventListeners[eventName];
+    callbacks?.forEach((callback) => callback(...payloads));
+  }
 }
